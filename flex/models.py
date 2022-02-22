cf8dd03aeee100ead82716f6fddabe8b9415e295
Nikodem Wróbel <nwrobel@bsbox.pl>
2022-02-22 10:31:04 +0100

    Add Images

--- flex/models.py
+++ flex/models.py
@@ -17,7 +17,8 @@ class FlexPage(Page):
         [
             ('title_and_text', blocks.TitleAndTextBlock()),
             ('full_richtext', blocks.RichTextBlock()),
-            ('simple_richtext', blocks.SimpleRichTextBlock())
+            ('simple_richtext', blocks.SimpleRichTextBlock()),
+            ('cards', blocks.CardBlock())
         ],
         null=True,
         blank=True,
--- streams/blocks.py
+++ streams/blocks.py
@@ -1,6 +1,7 @@
 """StreamFields live here"""
 
 from wagtail.core import blocks
+from wagtail.images.blocks import ImageChooserBlock
 
 
 class TitleAndTextBlock(blocks.StructBlock):
@@ -12,6 +13,29 @@ class TitleAndTextBlock(blocks.StructBlock):
         template = 'streams/title_and_text_block.html'
         icon = 'edit'
         label = 'Title & Text'
+
+
+class CardBlock(blocks.StructBlock):
+    """Cars with image and text/buttons"""
+
+    title = blocks.CharBlock(required=True, help_text='Add your title')
+
+    cards = blocks.ListBlock(
+        blocks.StructBlock(
+            [
+                ('image', ImageChooserBlock(required=True)),
+                ('title', blocks.CharBlock(required=True, max_length=40)),
+                ('text', blocks.TextBlock(required=True, max_length=200)),
+                ('button_page', blocks.PageChooserBlock(required=False)),
+                ('button_url', blocks.URLBlock(required=False, help_text='If the button page above is selected it will be used first.')),
+            ]
+        )
+    )
+
+    class Meta:
+        template = 'streams/card_block.html'
+        icon = 'placeholder'
+        label = 'Project cards'
 
 
 class RichTextBlock(blocks.RichTextBlock):
